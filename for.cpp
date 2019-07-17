f716f929f5c3ab4a28a8c07534e8ddea507ab5f6
Giancarlo Garcia <ggarciadeleon@csustan.edu>
2019-07-16 18:45:16 -0700

    Testing Returns

--- for.cpp
+++ for.cpp
@@ -17,8 +17,13 @@ int main(){
 	int n = 2;
 	int m = 1;
 	for(int i = 0; i < 10; i++){
-		int rtot = (n*ro) + (m*rp);
-		cout << "rtot is " << rtot << endl;
+		int rtot = (n*ro) + (m*rp) -1;
+		//cout << "rtot is " << rtot << endl;
+		if(i == 2){
+			return;
+			cout << i << '\n' << endl;
 	}
+	}
+
 
 }
