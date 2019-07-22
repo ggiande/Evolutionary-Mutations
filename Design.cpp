9799103c1e831e5912ebf7609e52c97f1e12fa55
Giancarlo Garcia <ggarciadeleon@csustan.edu>
2019-07-22 15:57:12 -0700

    Design.cpp

---
+++ Design.cpp
@@ -0,0 +1,23 @@
+//
+//	./for.out
+//
+
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <time.h>
+#include <vector>
+	
+using namespace std;
+int main(){
+	/* Given */
+	float ro = 2;
+	float rp = 1;
+	/* Design */
+	float Dec_By_Factor = 4;
+ 
+	float newRO = (ro/Dec_By_Factor);
+	float newRP = (rp/Dec_By_Factor);
+
+}
